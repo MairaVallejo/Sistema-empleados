d1527bbeb42d828700687444764d2bf76d5162f5
Maira Vallejo <vallejomaira218@gmail.com>
2021-10-30 19:08:56 -0300

    Creamos rutas para ver emplados

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, Request, redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ mysql = MySQL()
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'NO'
 app.config['MYSQL_DATABASE_DB'] = 'empleados'
 
 mysql.init_app(app)
@@ -17,12 +17,32 @@ def index():
     conn= mysql.connect()
     cursor = conn.cursor()
 
-    sql = "insert into empleados (nombre, correo, foto) values ('Juan','juan@mail.com','fofodejuan.jpg');"
+    sql = "SELECT * FROM empleados;"
     cursor.execute(sql)
 
+    empleados = cursor.fetchall() 
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados= empleados)
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/store', methods=["POST"])
+def store():
+    _nombre = Request.form['txtNombre']
+    _correo = Request.form['txtCorreo']
+    _foto = Request.form['txtFoto']
+
+    sql ="inset into empleados (nombre, correo, foto) values (%s,%s,%s);"
+    datos = (_nombre, _correo, _foto.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit() 
+    return redirect('/')
 
 if __name__== '__main__':
     app.run(debug=True)
